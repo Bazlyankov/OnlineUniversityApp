e88d4b1ff3165f490a05ed72b0e5a8e0640c00e3
Bazlyankov <georgimb@gmail.com>
2023-12-02 23:26:38 +0200

    Changed some old deprecated text.

--- src/main/java/com/studentsystemapp/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentsystemapp.repo;
-
-import com.studentsystemapp.model.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-
-}
